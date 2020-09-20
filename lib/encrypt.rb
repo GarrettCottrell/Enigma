ca5f6f35307edd6a7b314a8e1ae845a7d9fe364a
Garrett Cottrell <garrett.cottrell2116@gmail.com>
2020-09-20 10:18:09 -0600

    updated encrypt file for better formatting

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,7 +1,15 @@
-file = File.open(ARGV[0], "r")
-incoming_text = file.read
-file.close
+require './lib/enigma.rb'
+
+handle = File.open(ARGV[0], "r")
+
+incoming_text = handle.read
+
+handle.close
+
 downcased_text = incoming_text.downcase
+
 writer = File.open(ARGV[1], "w")
+
 writer.write(downcased_text.encrypt)
+
 writer.close
