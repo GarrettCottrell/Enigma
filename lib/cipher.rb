cd1c17d374cd31e0ea8b6b528a639c232052b06e
Garrett Cottrell <garrett.cottrell2116@gmail.com>
2020-09-21 17:59:15 -0600

    added in ability to handle special characters

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -11,26 +11,28 @@ class Cipher
   counter = 1
   alphabet_array = create_character_set
     message.each_char do |letter|
-      if counter == 1
+      if counter == 1 && alphabet_array.include?(letter)
         old_index = alphabet_array.find_index(letter)
         new_index = ((old_index) + (a_shift(key, date))) % (alphabet_array.count)
         encrypted_text << alphabet_array[new_index]
         counter += 1
-      elsif counter == 2
+      elsif counter == 2 && alphabet_array.include?(letter)
         old_index = alphabet_array.find_index(letter)
         new_index = ((old_index) + (b_shift(key, date))) % (alphabet_array.count)
         encrypted_text << alphabet_array[new_index]
         counter += 1
-      elsif counter == 3
+      elsif counter == 3 && alphabet_array.include?(letter)
         old_index = alphabet_array.find_index(letter)
         new_index = ((old_index) + (c_shift(key, date))) % (alphabet_array.count)
         encrypted_text << alphabet_array[new_index]
         counter += 1
-      elsif counter == 4
+      elsif counter == 4 && alphabet_array.include?(letter)
         old_index = alphabet_array.find_index(letter)
         new_index = ((old_index) + (d_shift(key, date))) % (alphabet_array.count)
         encrypted_text << alphabet_array[new_index]
         counter = 1
+      else
+        encrypted_text << letter
       end
     end
     encrypted_text
--- lib/decipher.rb
+++ lib/decipher.rb
@@ -11,30 +11,31 @@ class Decipher
   counter = 1
   alphabet_array = create_character_set
     message.each_char do |letter|
-      if counter == 1
+      if counter == 1 && alphabet_array.include?(letter)
         old_index = alphabet_array.find_index(letter)
         decrypted_shift = (alphabet_array.count) - (a_shift(key, date))
         decrypted_index = (old_index + decrypted_shift) % (alphabet_array.count)
         decrypted_text << alphabet_array[decrypted_index]
         counter += 1
-      elsif counter == 2
+      elsif counter == 2 && alphabet_array.include?(letter)
         old_index = alphabet_array.find_index(letter)
         decrypted_shift = (alphabet_array.count) - (b_shift(key, date))
         decrypted_index = (old_index + decrypted_shift) % (alphabet_array.count)
         decrypted_text << alphabet_array[decrypted_index]
         counter += 1
-      elsif counter == 3
+      elsif counter == 3 && alphabet_array.include?(letter)
         old_index = alphabet_array.find_index(letter)
         decrypted_shift = (alphabet_array.count) - (c_shift(key, date))
         decrypted_index = (old_index + decrypted_shift) % (alphabet_array.count)
         decrypted_text << alphabet_array[decrypted_index]
         counter += 1
-      elsif counter == 4
+      elsif counter == 4 && alphabet_array.include?(letter)
         old_index = alphabet_array.find_index(letter)
         decrypted_shift = (alphabet_array.count) - (d_shift(key, date))
         decrypted_index = (old_index + decrypted_shift) % (alphabet_array.count)
         decrypted_text << alphabet_array[decrypted_index]
         counter = 1
+      else decrypted_text << letter
       end
     end
     decrypted_text
