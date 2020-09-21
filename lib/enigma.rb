b0f465828db9b371d1e0cf1e237a406fde45d187
Garrett Cottrell <garrett.cottrell2116@gmail.com>
2020-09-20 20:47:03 -0600

    added note for decrypt

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -121,6 +121,9 @@ class Enigma
   encrypted_output
   end
 
+###### decrypt takes an encrypted message ######
+###### and the key and returns the original message ######
+
   def decrypt(message, key, date = get_current_date)
   decrypted_text = []
   counter = 1
@@ -161,22 +164,3 @@ class Enigma
   decrypted_output
   end
 end
-
-#   def decrypt(message, key, date = get_current_date)
-#     decrypted_text = []
-#     alphabet_array = create_character_set
-#       message.each_char do |letter|
-#         first_index = alphabet_array.find_index(letter)
-#         encrypt_index = ((first_index) + (shift(letter, key, date))) % (alphabet_array.count)
-#         decrypt_index = alphabet_array.count - encrypt_index
-#         decrypted_text << alphabet_array[decrypt_index]
-#     end
-#     decrypted_output = {}
-#
-#     decrypted_output[:decryption] = decrypted_text.join
-#     decrypted_output[:key] = key
-#     decrypted_output[:date] = date
-#
-#     decrypted_output
-#   end
-# end
