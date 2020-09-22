395a500fb12229973fb4d5161eac719031d59135
Garrett Cottrell <garrett.cottrell2116@gmail.com>
2020-09-21 18:27:41 -0600

    updated encrypt runner to account for key/dates given or not given

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -8,7 +8,15 @@ handle = File.open(ARGV[0], "r")
 
 incoming_text = handle.read
 
-new_text = enigma.encrypt(incoming_text.downcase.chomp)
+if ARGV[2] == nil
+  ARGV[2] = enigma.create_key
+end
+
+if ARGV[3] == nil
+  ARGV[3] = enigma.get_current_date
+end
+
+new_text = enigma.encrypt(incoming_text.downcase.chomp, ARGV[2], ARGV[3])
 
 handle.close
 
